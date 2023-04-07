087f7fbd08227e2e29187f6b847448f425667067
jamiema1 <jkmm.1024@gmail.com>
2023-04-06 23:53:23 -0700

    Added permissions to certain actions

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Axios from 'axios'
 import BoulderTable from './components/boulderTable/BoulderTable'
 import AddBoulder from './components/addBoulder/AddBoulder'
 import Introduction from './components/introduction/Introduction'
-// import BarChart from './components/BarChart'
+import BarChart from './components/BarChart'
 import 'chart.js'
 
 const serverhost = 'http://35.163.119.158:3001'
@@ -12,6 +12,12 @@ const localhost = 'http://localhost:3001'
 let hostname = localhost
 
 const local = true
+
+if (local) {
+  localStorage.setItem('adminStatus', 'true')
+} else {
+  localStorage.setItem('adminStatus', 'false')
+}
 
 function App () {
   const [boulderList, setBoulderList] = useState([])
@@ -24,48 +30,65 @@ function App () {
   }
 
   function addBoulderToDB (newBoulder) {
-    Axios.post(hostname + '/api/insert', newBoulder)
-      .then((response) => {
-        if (response.status != 200) {
-          alert('Failed to insert data with ' + response.data)
-          return
-        }
-        setBoulderList([newBoulder, ...boulderList])
-        boulderTableRef.current.updateBoulderList()
-      })
+    if (localStorage.getItem('adminStatus') == 'true') {
+      Axios.post(hostname + '/api/insert', newBoulder)
+        .then((response) => {
+          if (response.status != 200) {
+            alert('Failed to insert data with ' + response.data)
+            return
+          }
+          setBoulderList([newBoulder, ...boulderList])
+          boulderTableRef.current.updateBoulderList()
+        })
+    } else {
+      alert('You do not have access to adding')
+    }
   }
 
   function deleteBoulderFromDB (id) {
-    Axios.delete(hostname + '/api/delete', {data: {id: id}})
-      .then((response) => {
-        if (response.status != 200) {
-          alert('Failed to delete data with ' + response.data)
-          return
-        }
-        boulderTableRef.current.updateBoulderList();
-      })
+    if (localStorage.getItem('adminStatus') == 'true') {
+      Axios.delete(hostname + '/api/delete', {data: {id: id}})
+        .then((response) => {
+          if (response.status != 200) {
+            alert('Failed to delete data with ' + response.data)
+            return
+          }
+          boulderTableRef.current.updateBoulderList();
+        })
+    } else {
+      alert('You do not have access to deleting')
+    }
   }
 
   function getBoulderListFromDB (uri) {
-    Axios.get(hostname + '/api/get?' + uri)
-      .then((response) => {
-        if (response.status != 200) {
-          alert('Failed to get data with ' + response.data)
-          return
-        }
-        setBoulderList(response.data)
-      })
+    
+    if (localStorage.getItem('adminStatus') == 'true' || 
+      localStorage.getItem('adminStatus') == 'false') {
+      Axios.get(hostname + '/api/get?' + uri)
+        .then((response) => {
+          if (response.status != 200) {
+            alert('Failed to get data with ' + response.data)
+            return
+          }
+          setBoulderList(response.data)
+        })
+    } else {
+      alert('You do not have access to getting')
+    }
   }
 
   function updateBoulderFromDB (updatedBoulder) {
-    Axios.put(hostname + '/api/update', updatedBoulder)
-      .then(() => {
-        boulderTableRef.current.updateBoulderList();
-        // alert('Successful Update')
-      })
-      .catch(() => {
-        alert('Failed Update')
-      })
+    if (localStorage.getItem('adminStatus') == 'true') {
+      Axios.put(hostname + '/api/update', updatedBoulder)
+        .then(() => {
+          boulderTableRef.current.updateBoulderList();
+        })
+        .catch(() => {
+          alert('Failed Update')
+        })
+    } else {
+      alert('You do not have access to updating')
+    }
   }
   return (
     <>
@@ -82,7 +105,7 @@ function App () {
         getBoulderListFromDB = { getBoulderListFromDB }
         ref = {boulderTableRef}
       />
-      {/* <BarChart boulderList={boulderList}/> */}
+      <BarChart boulderList={boulderList}/>
     </>
   )
 }
